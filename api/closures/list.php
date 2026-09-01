2faac0e88ab4edd53bd584b64200df7b4345e604
FOaMN_NR <Foamnonthawat@gmail.com>
2026-09-01 15:30:50 +0700

    fix: correct include paths in closures API endpoints

--- api/closures/list.php
+++ api/closures/list.php
@@ -1,6 +1,9 @@
 <?php
-require_once '../../config/Database.php';
-require_once '../../controllers/CourtClosureController.php';
+require_once __DIR__ . '/../../models/BaseModel.php';
+require_once __DIR__ . '/../../models/CourtModel.php';
+require_once __DIR__ . '/../../models/CourtClosureModel.php';
+require_once __DIR__ . '/../../controllers/BaseController.php';
+require_once __DIR__ . '/../../controllers/CourtClosureController.php';
 
 $controller = new CourtClosureController();
 $controller->list();
